7bedcb77ad71e5baaf40b9d97c94e3befcb943af
Jwilv <100983338+Jwilv@users.noreply.github.com>
2022-06-11 20:34:18 -0300

    Update tp con leandro.ino

--- tp con leandro.ino
+++ tp con leandro.ino
@@ -14,78 +14,101 @@ int pulsadores[4] = {
     {PIN_PULSADOR_MENU, PIN_PULSADOR_SALIR, PIN_PULSADOR_INCREMENTAR, PIN_PULSADOR_DECREMENTAR}};
 int leds[3] = {
     {PIN_LED_R, PIN_LED_G, PIN_LED_B}};
-    int periodo1 = 4000;
-    int periodo2 = 8000;
+int periodo1 = 4000;
+int periodo2 = 8000;
 enum std_menu = {
     HOME,
     LED_R,
     LED_G,
     LED_B};
 int menu = HOME;
-enum logs = {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-int index = 0;
+enum pulsadores = {
+    btnMenu,
+    btnSalir,
+    btnIncrementar,
+    btnDecrementar};
+void delacracionPines()
+{
+    for (int fila1 = 0; fila1 < 4; fila1++)
+    {
+        int pinPulsador = pulsadores[fila1];
+        pinMode(pinPulsador, INPUT);
+    }
+    for (int fila2 = 0; fila2 < 3; fila2++)
+    {
+        int pinLed = leds[fila2];
+        pinMode(pinLed, OUTPUT);
+    }
+}
 void setup()
 {
-    
+    Serial.begin(9600);
+    delacracionPines();
 }
 
-
-
-void actuadores(){
-    //mostrar bomba de abua, ventilador, intencidad luz del 0% al 100%
+void actuadores()
+{
+    // mostrar bomba de abua, ventilador, intencidad luz del 0% al 100%
 }
-void sensores(){
-    //mostrar sensores, tempratura ambiente, temp agua en c, sensor luz ldr en lux 
+void sensores()
+{
+    // mostrar sensores, tempratura ambiente, temp agua en c, sensor luz ldr en lux
 }
-void home (int periodo,int periodo_2)
-{ int inActua = 0;
-  int inSen = 0;
-    if(millis() > inActua + periodo){
+void home(int periodo, int periodo_2)
+{
+    int inActua = 0;
+    int inSen = 0;
+    if (millis() > inActua + periodo)
+    {
         inActua = millis();
         actuadores();
     }
-    else if(millis() > inSen + periodo_2){
+    else if (millis() > inSen + periodo_2)
+    {
         inSen = millis();
         sensores();
     }
-
 }
-
 
 switch (menu)
 {
 case HOME:
-{   logEvento("HOME");//falta crear la funcion de historial
-    home(periodo1,periodo2);
+{
+    logEvento("HOME"); // falta crear la funcion de historial
+    home(periodo1, periodo2);
     break;
 }
-case MENU :{
+case MENU:
+{
     logEvento("MENU");
     printMenu();
     break;
 }
 case LED_R:
-{   logEvento("LED_RED");
-    led_r();//mostrar lo mismo pero indicando lugar y opcion para modificar parametros
+{
+    logEvento("LED_RED");
+    led_r(); // mostrar lo mismo pero indicando lugar y opcion para modificar parametros
     break;
 }
 case LED_G:
-{  logevento("LED_GREEN");
+{
+    logevento("LED_GREEN");
     led_g();
     break;
 }
 case LED_B:
-{ logEvento("LED_BLUE");
+{
+    logEvento("LED_BLUE");
     led_b();
     break;
 }
-case LOG_EVENTOS:{
-    //MOSTRAR EVENTOS
+case LOG_EVENTOS:
+{
+    // MOSTRAR EVENTOS
     break;
 }
 }
 
-void loop{
-
+void loop
+{
 }
